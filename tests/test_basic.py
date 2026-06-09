81f04c5469abdaba208f924959e1b60f8976f0f8
Habib ur Rehmaan <h.rehmaan96@gmail.com>
2026-06-09 17:43:19 +0200

    version updated

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -39,7 +39,7 @@ from simfix.system import (
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
 
 
 def test_analyze_python_repo(tmp_path: Path) -> None:
